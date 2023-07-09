20f5899b02517f0762611b92eb87a44876de5ee7
Andrés Francisco almarza menjiba <francisco.almarza@gmail.com>
2023-07-09 12:03:56 -0400

    configuraciones para deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tarea-9-front-end/',
+  base: 'https://neroangello.github.io/tarea-9-front-end/',
 })
